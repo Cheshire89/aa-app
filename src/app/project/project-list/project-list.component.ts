1f2ceeec55ba8b57019b9239eb5e6e1b0a10dc78
Aleksandr Antonov <antonov.a.aleksandr@gmail.com>
2024-11-16 10:19:23 -0700

    link up links

--- src/app/project/project-list/project-list.component.ts
+++ src/app/project/project-list/project-list.component.ts
@@ -1,8 +1,8 @@
-import { Component, ElementRef } from '@angular/core'
-import { Router } from '@angular/router'
+import { Component, QueryList, ViewChildren } from '@angular/core'
 import { EnvironmentService } from '@shared/services/environment/environment.service'
 import { Observable } from 'rxjs'
 import { Company } from 'src/app/models/company.model'
+import { Project } from 'src/app/models/project.model'
 import VanillaTilt from 'vanilla-tilt'
 
 @Component({
@@ -11,28 +11,21 @@ import VanillaTilt from 'vanilla-tilt'
     styleUrl: './project-list.component.scss',
 })
 export class ProjectListComponent {
+    @ViewChildren('workDetail') workDetail: QueryList<any>;
+
     projects$: Observable<Company[]>
 
     constructor(
-        private ele: ElementRef,
-        private router: Router,
         private _env: EnvironmentService
     ) {
         this.projects$ = this._env.getFile('projects.json')
     }
 
     ngAfterViewInit(): void {
-        const ele = this.ele.nativeElement.querySelectorAll(
-            '.work__project-detail'
-        )
-        VanillaTilt.init(ele, { max: 25, speed: 400 })
+        VanillaTilt.init(this.workDetail.map(e => e.nativeElement), { max: 25, speed: 400 })
     }
 
-    onViewCompany(company: Company) {
-        // this.router.navigate([company.slug])
-        console.warn(
-            'Company details page have not been implimented yet',
-            `[${company.slug}]`
-        )
+    onViewProject(project: Project) {
+        window.open(project.link, '_blank')
     }
 }
